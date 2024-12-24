a9639b47c18e699077e88397f1191722905f0298
Daiyaz <uchihachikk3@gmail.com>
2024-12-24 15:00:23 +0700

    Add Fibonacci function to calculate Fibonacci numbers

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
+
+int fibonacci(int y);
+
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int y){
+    
+    if(y==0)return 0;
+    if(y==1) return 1;
+    return fibonacci(y-1) + fibonacci(y-2);
 }
